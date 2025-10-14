2ba98caf334166941f271297746d8eb398cdf8a5
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-10-14 03:14:57 -0700

    description

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 http.createServer((req, res) => {
     fs.readFile('HTML/web.html','utf-8',(error,data)=>{
         if(error){
-            res.writeHead(500,{'Content-Type':'text/plain'});
+            res.writeHead(500,{'Content-Type':'text/plain'});//500 means internal server error
             res.write("Internal Server Error");
             res.end();
         }
@@ -13,3 +13,9 @@ http.createServer((req, res) => {
 
     })
 }).listen(5000);
+//fs--->file system module
+//readFile--->to read the file
+//utf-8--->encoding format that is used to read the file
+//error--->if any error occurs while reading the file it will be stored in this variable
+//data--->the content of the file will be stored in this variable
+//writeHead--->to write the header of the response

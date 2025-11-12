df1c321ce49316030e55e9e132715d561095fd50
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-11-12 19:02:56 +0500

    server_response

--- server_response.js
+++ server_response.js
@@ -1,9 +1,9 @@
 //What is server response?
-//When you make a request to a server (like when you visit a website), the server sends back a response. This response includes information like the status of your request (was it successful or not?),
+// When you make a request to a server (like when you visit a website), the server sends back a response. This response includes information like the status of your request (was it successful or not?),
 // the data you asked for (like a webpage or some information), and other details about how to handle that data.
 
 const http = require("http");
-const age =21;
+const age =  21;
 http.createServer((req,res)=>{
     res.setHeader("Content-Type","text/html");
     res.write(`
